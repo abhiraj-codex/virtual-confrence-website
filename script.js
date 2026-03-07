0480af76b276e22e4e74259afef0dc23d71332f1
01abhirajkumar01-creator <01.abhiraj.kumar.01@gmail.com>
2026-03-07 13:46:21 +0530

    Done

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const hamMenu = document.querySelector('.bx.bx-menu-right');
 const closeMEnu = document.querySelector('.bx.bx-x');
 const navBar = document.querySelector('.navbar');
+const logo = document.querySelector('.logo')
 
 function just(){
     hamMenu.classList.toggle('active');
     closeMEnu.classList.toggle('active');
  navBar.classList.toggle('active');
+ logo.classList.toggle('active')
 
 }
