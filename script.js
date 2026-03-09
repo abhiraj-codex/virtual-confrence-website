149f13d907cfb37da8b48fc0288e192468a7686f
01abhirajkumar01-creator <01.abhiraj.kumar.01@gmail.com>
2026-03-09 20:51:43 +0530

    Final Commit For the Eod

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ function just(){
  logo.classList.toggle('active')
 
 }
+
+
+// Chat gpt Generated
+
+ const header = document.querySelector("header");
+
+window.addEventListener("scroll", function () {
+    if(window.scrollY > 50){
+        header.classList.add("scrolled");
+    } else{
+        header.classList.remove("scrolled");
+    }
+});   
